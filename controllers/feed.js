dc0145fa16ca9b6af4ef67d00713c28b7d1ef6c3
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-07 15:14:38 +0200

    Add the edit post logic to the controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -78,6 +78,14 @@ exports.getPost = (req, res, next) => {
 
 exports.updatePost = (req, res, next) => {
   const postId = req.params.postId;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
   const title = req.body.title;
   const content = req.body.content;
   let imageUrl = req.body.image;
@@ -89,4 +97,29 @@ exports.updatePost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+
+      return post.save();
+    })
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: 'Post updated successfully.', post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
