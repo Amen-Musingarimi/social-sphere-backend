3727f2d2254fd947485d817c4031705aa39858b7
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-07 15:49:55 +0200

    Add delete image function when post ois edited

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
@@ -105,6 +107,9 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
       post.title = title;
       post.imageUrl = imageUrl;
       post.content = content;
@@ -122,4 +127,9 @@ exports.updatePost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
