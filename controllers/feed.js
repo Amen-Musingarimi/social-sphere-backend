7e3045d81af84db2662d41372315f6fa09a1fa02
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-09 11:53:22 +0200

    Add sort method to get post to sort posts correctly

--- controllers/feed.js
+++ controllers/feed.js
@@ -17,6 +17,7 @@ exports.getPosts = (req, res, next) => {
       totalItems = count;
       return Post.find()
         .populate('creator')
+        .sort({ createdAt: -1 })
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
